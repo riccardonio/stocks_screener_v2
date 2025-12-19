4ef48dd7e7929fe594fc1e3b6c98fe217b82d712
riccardonio <riccapare@gmail.com>
2025-12-19 21:56:18 +0100

    slightly change the layout

--- app/main_app_screener.py
+++ app/main_app_screener.py
@@ -17,6 +17,20 @@ st.set_page_config(
     initial_sidebar_state="auto",
 )
 
+st.markdown(
+    """
+        <style>
+               .block-container {
+                    padding-top: 1rem;
+                    padding-bottom: 0rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+        </style>
+        """,
+    unsafe_allow_html=True,
+)
+
 st.title("Magic Screener")
 available_tickers = os.listdir(FMP_DATA_DIR)
 available_tickers.sort()
